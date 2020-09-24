06a5fa988a466de0edb570e207392c857923814f
vikash rahii <vikashrahii999@gmail.com>
2020-09-25 04:21:53 +0530

    schema created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+const Profile = require("../../models/profile");
+const User = require("../../models/User");
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await profile
+      .findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "No user found" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("srever error");
+  }
+});
 
 module.exports = router;
